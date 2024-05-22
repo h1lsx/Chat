291d68d165b6fe0244a5cbfe9d4b389cea95034a
h1lsx <139397050+h1lsx@users.noreply.github.com>
2024-05-22 22:58:14 +0000

    updated for platformer

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var select;
 var chat;
 function addChat(user, text) {
-  document.getElementById("chatbody").innerHTML += `<div>${user}:</div><div>${text}</div>`
+  document.getElementById("chatbody").innerHTML += `<div>${user}:</div><div>${text}</div><br>`
 }
 async function load() {
   select = document.getElementById("chat");
   chat = select.value;
-  $.get(`https://json-h1lsx.koyeb.app/chat${chat}`).then(function (d) {
+  $.get(`https://json-h1lsx-ea20d84e.koyeb.app/chat${chat}`).then(function (d) {
+      //document.getElementById("chatbody").innerHTML = "";
       document.getElementById("chatbody").innerHTML = "";
       d.forEach(a => addChat(a.user, a.message));
     })
@@ -20,7 +21,7 @@ async function submit() {
   let message = document.getElementById("text").value;
   if(user == "delete4576") {
     $.ajax({
-    url: `https://json-h1lsx.koyeb.app/chat${chat}/${message}`,
+    url: `https://json-h1lsx-ea20d84e.koyeb.app/chat${chat}/${message}`,
     type: 'DELETE',
     success: function(result) {
         alert(`Deleted Message Id ${message}`)
@@ -31,7 +32,7 @@ async function submit() {
       "message": message,
       "user": user
     }
-    $.post(`https://json-h1lsx.koyeb.app/chat${chat}`, body, (d, status) => {
+    $.post(`https://json-h1lsx-ea20d84e.koyeb.app/chat${chat}`, body, (d, status) => {
       alert("Chat Updated!");
       load();
     }).fail((d, status) => {
